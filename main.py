12fe0250d8eed928a25760098bc31339a66ee492
pengxiangbobin <pengxiangbobin@163.com>
2024-06-04 15:23:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,11 +9,13 @@ my_train, my_val = trainGenerator(16, 4, 'g:/RVE_pre/train',
                                     'image','mask', train_gen_args, val_gen_args)
 
 model_checkpoint = ModelCheckpoint('g:/RVE_pre/xxx.hdf5', monitor='val_loss',verbose=1, save_best_only=True)
-#tensorboard = TensorBoard(log_dir='g:/RVE_pre/log')
+#tensorboard = TensorBoard(log_dir='g:/RVE_pre/log', histogram_freq=1, embeddings_freq=1)
 reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=4, min_lr=0)
 callbacks_list = [model_checkpoint, reduce_lr]
 
 model = unet()
+#two-stage training
+#model = load_model('g:/RVE_pre/xxx.hdf5')
 
 history = model.fit(my_train,
                     validation_data=my_val,
